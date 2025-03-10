2c5c3ec52231a5316b42c19c2670c9fab32d204a
mvivy3 <meredithyen@usf.edu>
2025-03-09 23:17:20 -0400

    Task 2 - Employee Cards Dynamic Addition

---
+++ cc_13.js
@@ -0,0 +1,33 @@
+// TASK 2: ADDING EMPLOYEE CARDS DYNAMICALLY 
+
+function addEmployeeCard(name, position) { // function to create employee card 
+    const employeeContainer = document.getElementById(`employeeContainer`) // create element for employee card
+    const card = document.createElement(`div`);
+    card.setAttribute("class", "employee-card");
+    card.classList.add(`employee-card`);
+
+    // Created heading of employee card
+    const employeeName = document.createElement(`h2`);
+    employeeName.textContext = name;
+    //Created paragraph for employee position
+    const employeePosition = document.createElement('p');
+     employeePosition.textContent=position;
+ 
+    // Created the remove button
+     const removeButton = document.createElement('button');
+     removeButton.textContent = 'Remove';
+     removeButton.setAttribute("class","remove-button");
+     removeButton.addEventListener('click',()=>{
+         employeeContainer.removeChild(card);
+     });
+ 
+     card.appendChild(employeeName); // heading created
+     card.appendChild(employeePosition); // paragraph created
+     card.appendChild(removeButton); // remove button for deleting the card
+     employeeContainer.appendChild(card);
+ 
+ };
+ 
+ // Test Cases
+ addEmployeeCard("Edward Paisley","F1 driver");
+ addEmployeeCard("Luciendo Garlawry","F1 presenter");
